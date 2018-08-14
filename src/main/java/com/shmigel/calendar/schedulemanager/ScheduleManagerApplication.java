e90decd816f8e824d363e185e752555ff75affd7
Shmigel <shmigelvolodimir@gmail.com>
2018-08-14 18:08:57 +0300

    Test two ways

--- src/main/java/com/shmigel/calendar/schedulemanager/ScheduleManagerApplication.java
+++ src/main/java/com/shmigel/calendar/schedulemanager/ScheduleManagerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +31,122 @@ public class ScheduleManagerApplication {
 	public ResponseEntity<?> commonResponse(@RequestBody String body) {
 //		System.out.println(body);
 		logger.info("Receive request with body: {}", body);
-		return ResponseEntity.ok().body("It's working");
+		return ResponseEntity.ok().body("{\n" +
+				"  \"fulfillmentText\": \"This is a text response\",\n" +
+				"  \"fulfillmentMessages\": [\n" +
+				"    {\n" +
+				"      \"card\": {\n" +
+				"        \"title\": \"card title\",\n" +
+				"        \"subtitle\": \"card text\",\n" +
+				"        \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
+				"        \"buttons\": [\n" +
+				"          {\n" +
+				"            \"text\": \"button text\",\n" +
+				"            \"postback\": \"https://assistant.google.com/\"\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    }\n" +
+				"  ],\n" +
+				"  \"source\": \"example.com\",\n" +
+				"  \"payload\": {\n" +
+				"    \"google\": {\n" +
+				"      \"expectUserResponse\": true,\n" +
+				"      \"richResponse\": {\n" +
+				"        \"items\": [\n" +
+				"          {\n" +
+				"            \"simpleResponse\": {\n" +
+				"              \"textToSpeech\": \"this is a simple response\"\n" +
+				"            }\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    },\n" +
+				"    \"facebook\": {\n" +
+				"      \"text\": \"Hello, Facebook!\"\n" +
+				"    },\n" +
+				"    \"slack\": {\n" +
+				"      \"text\": \"This is a text response for Slack.\"\n" +
+				"    }\n" +
+				"  },\n" +
+				"  \"outputContexts\": [\n" +
+				"    {\n" +
+				"      \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
+				"      \"lifespanCount\": 5,\n" +
+				"      \"parameters\": {\n" +
+				"        \"param\": \"param value\"\n" +
+				"      }\n" +
+				"    }\n" +
+				"  ],\n" +
+				"  \"followupEventInput\": {\n" +
+				"    \"name\": \"event name\",\n" +
+				"    \"languageCode\": \"en-US\",\n" +
+				"    \"parameters\": {\n" +
+				"      \"param\": \"param value\"\n" +
+				"    }\n" +
+				"  }\n" +
+				"}");
 	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ResponseEntity<?> commonPOSTResponse(@RequestBody String body) {
+//		System.out.println(body);
+		logger.info("Receive request with body: {}", body);
+		return ResponseEntity.ok().body("{\n" +
+				"  \"fulfillmentText\": \"This is a post response\",\n" +
+				"  \"fulfillmentMessages\": [\n" +
+				"    {\n" +
+				"      \"card\": {\n" +
+				"        \"title\": \"post title\",\n" +
+				"        \"subtitle\": \"post text\",\n" +
+				"        \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
+				"        \"buttons\": [\n" +
+				"          {\n" +
+				"            \"text\": \"post text\",\n" +
+				"            \"postback\": \"https://assistant.google.com/\"\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    }\n" +
+				"  ],\n" +
+				"  \"source\": \"example.com\",\n" +
+				"  \"payload\": {\n" +
+				"    \"google\": {\n" +
+				"      \"expectUserResponse\": true,\n" +
+				"      \"richResponse\": {\n" +
+				"        \"items\": [\n" +
+				"          {\n" +
+				"            \"simpleResponse\": {\n" +
+				"              \"textToSpeech\": \"this is a simple post response\"\n" +
+				"            }\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    },\n" +
+				"    \"facebook\": {\n" +
+				"      \"text\": \"Hello, Facebook!\"\n" +
+				"    },\n" +
+				"    \"slack\": {\n" +
+				"      \"text\": \"This is a text response for Slack.\"\n" +
+				"    }\n" +
+				"  },\n" +
+				"  \"outputContexts\": [\n" +
+				"    {\n" +
+				"      \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
+				"      \"lifespanCount\": 5,\n" +
+				"      \"parameters\": {\n" +
+				"        \"param\": \"param value\"\n" +
+				"      }\n" +
+				"    }\n" +
+				"  ],\n" +
+				"  \"followupEventInput\": {\n" +
+				"    \"name\": \"event name\",\n" +
+				"    \"languageCode\": \"en-US\",\n" +
+				"    \"parameters\": {\n" +
+				"      \"param\": \"param value\"\n" +
+				"    }\n" +
+				"  }\n" +
+				"}");
+	}
+
 }
